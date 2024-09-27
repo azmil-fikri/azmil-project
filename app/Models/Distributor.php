e6417ac8d22405182359e715c9770b5028e4c7a6
Azmil Fikri <azmilfikri757@gmail.com>
2024-09-27 18:49:18 +0700

    first commit

---
+++ app/Models/Distributor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Distributor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_dsitributor', 'lokasi', 'kontak', 'email'
+    ];
+}
